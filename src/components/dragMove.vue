47c7902e0e2cd097855020dc5c44c9bba2cd1efe
wangzeng <wangzeng@jybdshop.cn>
2019-08-09 17:02:15 +0800

    更新进度

--- src/components/dragMove.vue
+++ src/components/dragMove.vue
@@ -1,9 +1,8 @@
 <!-- 组件库 drag-move -->
 <template>
   <div id="drag-wrapper" v-if="data">
-    <div v-for="(item,index) in data" :key="index" :style="{postion:'absolute'}" :class="customClass">
+    <div v-for="(item,index) in data" :key="index" @mousedown="elMousedown" style="position:absolute" :style="countStyle(item)" class="drag-el" :class="customClass">
       <slot></slot>
-      {{item.text}}
     </div>
   </div>
 
@@ -19,11 +18,7 @@ export default {
     },
     data: {
       type: Array,
-      default: () => [
-        {
-          text: "123"
-        }
-      ]
+      default: () => []
     },
     customClass: {
       type: String,
@@ -43,21 +38,107 @@ export default {
     }
   },
   data() {
-    return {};
+    return {
+      docWidth: document.body.clientWidth,
+      docHeight: document.body.clientHeight
+    };
   },
+  // directives: {
+  //   dragMouse(el) {
+  //     let e = el; // 获取当前元素
 
+  //   }
+  // },
   components: {},
 
-  computed: {},
+  computed: {
+    countBaseW() {
+      let docWidth = this.docWidth,
+        colNums = this.colNums <= 0 ? 1 : this.colNums;
+      return docWidth / colNums;
+    }
+  },
 
   mounted() {},
 
-  methods: {}
+  methods: {
+    // 计算元素得宽高
+    countStyle(item) {
+      let { x, y, w, h } = item;
+      // 获取基础宽度
+      let getBaseWidth = this.countBaseW;
+      x = x && x >= 0 ? x : 0;
+      y = y && y >= 0 ? y : 0;
+      w = w && w > 0 ? w : 1;
+      h = h && h > 0 ? h : 1;
+      let obj = {
+        width: getBaseWidth * w + "px",
+        height: this.rowHeight * h + "px",
+        top: 0,
+        left: 0
+      };
+      return obj;
+    },
+    // 计算当前位置是否已经占用
+    isOccupy() {},
+    /** 移动方法 start**/
+    // 元素选中
+    elMousedown(event) {
+      console.log(event);
+      let elTarget = event.target;
+      let domOffsetX = event.offsetX || event.layerX,
+        domOffsetY = event.offsetY || event.layerY;
+      let disX = event.clientX - domOffsetX,
+        disY = event.clientY - domOffsetY;
+      // let disX = parseFloat(elTarget.style.top),
+      //   disY = parseFloat(elTarget.style.left);
+      console.log("元素top:", elTarget.style.top);
+      console.log("元素left:", elTarget.style.left);
+      document.onmousemove = e => {
+        let top = e.clientY - disY,
+          left = e.clientX - disX;
+        console.log("正在移动");
+        console.log("offsetX", e.offsetX);
+        console.log("offsetY", e.offsetY);
+        elTarget.style.top = top + "px";
+        elTarget.style.left = left + "px";
+      };
+      document.onmouseup = e => {
+        // 防止鼠标弹起来得时候不再移动
+        document.onmousemove = null;
+        // 防止鼠标弹起来后还会循环（即预防鼠标放上去的时候还会移动）
+        document.onmouseup = null;
+      };
+    },
+    // 元素移动中
+    elMousemove(event) {
+      console.log("开启移动");
+      console.log(this.disX);
+      console.log(this.disY);
+    },
+    // 元素移动结束
+    elMouseover(event) {}
+    /** 移动方法 end **/
+  }
 };
 </script>
 <style>
 #drag-wrapper {
   position: relative;
+  /* overflow: hidden; */
+  width: 100%;
+  height: 200px;
+}
+#drag-wrapper::after {
+  content: " ";
+  height: 100%;
+  width: 100%;
+  clear: both;
+  box-sizing: border-box;
+  display: block;
+}
+.drag-el {
+  cursor: move;
 }
 .drag-move_chunk {
   border: 1px solid #dedede;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,33 @@
 <template>
   <div>
-     <drag-move></drag-move>
+    <drag-move :data="data">
+      456132
+    </drag-move>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 export default {
-  name: 'home',
-  components: {
-  }
-}
+  name: "home",
+  data() {
+    return {
+      data: [
+        {
+          x: 0,
+          y: 0,
+          w: 1,
+          h: 1
+        },
+        {
+          x: 1,
+          y: 0,
+          w: 1,
+          h: 1
+        }
+      ]
+    };
+  },
+  components: {}
+};
 </script>
